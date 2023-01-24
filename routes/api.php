20f413bac367a24072ad4c83e651bfc8c7230028
DMITRII1548 <korsunov2006@list.ru>
2023-01-24 19:49:08 +0600

    sanctum and fortiry added

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+
 });
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/get', GetController::class);
+});
+
+
+
