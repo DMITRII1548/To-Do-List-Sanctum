e2277d73d18521d78cd7a937b281f23e40cbf3ff
DMITRII1548 <korsunov2006@list.ru>
2023-01-25 15:49:15 +0600

    issue-3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import './bootstrap'
+import '../sass/app.scss'
 import { createApp } from 'vue'
 import AppComponent from './components/App.vue'
 import router from './router'
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-<div id="app">
+<div id="app" class="container">
     <app-component></app-component>
 </div>
 
