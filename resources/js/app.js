acf712586fd6be6f20050496412fbb61e60e2580
DMITRII1548 <korsunov2006@list.ru>
2023-01-25 15:12:50 +0600

    vue and vue-loader added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-import './bootstrap';
+import './bootstrap'
+import { createApp } from 'vue'
+import AppComponent from './components/App.vue'
+import router from './router'
+
+
+const app = createApp({
+    components: {
+        AppComponent,
+    }
+})
+
+app.use(router)
+app.mount('#app')
---
+++ resources/js/components/User/Login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <input v-model="email" type="email" placeholder="email">
+        <input v-model="password" type="password" placeholder="password">
+        <button @click.prevent="login()" type="submit">Login</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+
+    methods: {
+        login() {
+            axios.get('/sanctum/csrf-cookie')
+                .then(response => {
+                    axios.post('/login', { email: this.email, password: this.password })
+                        .then(res => {
+                            console.log(res)
+                        })
+                });
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
