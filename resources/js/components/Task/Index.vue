110ba8b40e0f8b6161c90d5225af5f77d0f19289
DMITRII1548 <korsunov2006@list.ru>
2023-01-28 16:06:34 +0600

    destroy action added

--- resources/js/components/Task/Index.vue
+++ resources/js/components/Task/Index.vue
@@ -5,14 +5,20 @@
                 <tr>
                     <th scope="col">№</th>
                     <th scope="col">Title</th>
-                    <th scope="col">Actions</th>
+                    <th scope="col">Show</th>
+                    <th scope="col">Delete</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="task in tasks">
                     <th scope="row">{{ task.id }}</th>
                     <td>{{ task.title }}</td>
-                    <td><router-link :to="{ name: 'task.show', params: { id: task.id } }" class="btn btn-primary">Open</router-link></td>
+                    <td>
+                        <router-link :to="{ name: 'task.show', params: { id: task.id } }" class="btn btn-primary">Open</router-link>
+                    </td>
+                    <td>
+                        <a @click.prevent="confirmActionDeleteTask(task.id)" href="#" class="btn btn-danger ml-1">Delete</a>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -40,11 +46,23 @@ export default {
                     this.tasks = res.data.data
                 })
         },
+
+        destroyTask(id) {
+            axios.delete(`/api/tasks/${id}`)
+                .then(res => {
+                    this.getTasks()
+                })
+        },
+
+        confirmActionDeleteTask(id) {
+            if (confirm('Are you wanting to delete this task')) {
+                this.destroyTask(id)
+            }
+        }
     },
 
 }
 </script>
 
 <style scoped>
-
 </style>
