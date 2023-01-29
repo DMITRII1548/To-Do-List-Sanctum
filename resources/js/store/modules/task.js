b0e9333e7924d2c85b866e25def57d9cc1dba5d2
Dmitrii <korsunov2006@list.ru>
2023-01-29 12:32:44 +0600

    Index component imported in vuex

--- resources/js/store/modules/task.js
+++ resources/js/store/modules/task.js
@@ -1,19 +1,45 @@
 import router from "../../router"
 
 const state = {
-
+    tasks: [],
+    task: []
 }
 
 const actions = {
+    getTasks({ commit }) {
+        axios.get('/api/tasks')
+            .then(res => {
+                commit('setTasks', res.data.data)
+            })
+    },
 
+    destroyTask({dispatch}, id) {
+        axios.delete(`/api/tasks/${id}`)
+            .then(res => {
+                this.dispatch('getTasks')
+            })
+    },
+
+    confirmActionDeleteTask({dispatch}, id) {
+        if (confirm('Are you really wanting to delete this task')) {
+            this.dispatch('destroyTask', id)
+        }
+    }
 }
 
 const getters = {
-
+    tasks: state => state.tasks,
+    task: state => state.task
 }
 
 const mutations = {
+    setTask(state, task) {
+        state.task = task
+    },
 
+    setTasks(state, tasks) {
+        state.tasks = tasks
+    }
 }
 
 export default {
